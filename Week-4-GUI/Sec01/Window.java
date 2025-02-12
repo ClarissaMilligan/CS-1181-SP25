92349a3d3d62e576e8ca604b15d0f4ee1bc77efb
Clarissa Milligan <milligan.s.clarissa@gmail.com>
2025-02-12 12:10:58 -0500

    GUI code

---
+++ Week-4-GUI/Sec01/Window.java
@@ -0,0 +1,136 @@
+import javax.swing.*;
+import javax.swing.plaf.BorderUIResource;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Window extends JFrame
+{
+    private int countClicks = 0;
+    private JPanel contentPane = new JPanel();
+    private JLabel lab = new JLabel();
+    private JButton one = new JButton("one");
+    private JButton two = new JButton("two");
+    private JButton three = new JButton("three");
+
+    public Window(String title)
+    {
+        super(title);
+        this.setSize(500, 500);
+        this.setLocation(0, 100);
+        this.setContentPane(contentPane);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        countClicks = 0;
+        lab.setText("Hello, I'm a label!");
+        lab.setFont(new Font("Comic Sans MS", 0, 50));
+        customLayout();
+        //gridLayout();
+        //boxLayout();
+        //borderLayout();
+        //flowLayout();
+
+        class InnerButtonListen implements ActionListener
+        {
+            @Override
+            public void actionPerformed(ActionEvent e)
+            {
+                System.out.println("I was pressed!");
+                lab.setFont(new Font("Comic Sans MS", 0, 50));
+            }
+        }
+        one.addActionListener(new InnerButtonListen());
+
+        two.addActionListener(new ActionListener()
+        {
+            @Override
+            public void actionPerformed(ActionEvent e)
+            {
+                System.out.println("I was pressed!");
+                lab.setFont(new Font("Comic Sans MS", 1, 30));
+            }
+        });
+
+        three.addActionListener(e ->
+        {
+            System.out.println("I was pressed!");
+            lab.setFont(new Font("Comic Sans MS", 2, 70));
+        });
+
+//        JButton buttonOne = new JButton("Click me!");
+//        JButton buttonTwo = new JButton("No! Click ME!");
+//
+//        buttonOne.setPreferredSize(new Dimension(30, 30));
+//
+//        contentPane.add(buttonOne);
+//        contentPane.add(buttonTwo);
+
+        this.setVisible(true);
+    }
+
+    private void flowLayout()
+    {
+        for (int i = 0; i < 10; i++)
+        {
+            this.contentPane.add(new JButton("Button #" + i));
+        }
+    }
+
+    private void borderLayout()
+    {
+        this.contentPane.setLayout(new BorderLayout());
+
+        JPanel northPanel = new JPanel();
+
+        northPanel.add(new JButton("FILE"));
+        northPanel.add(new JButton("EDIT"));
+
+        this.contentPane.add(northPanel, BorderLayout.NORTH);
+        this.contentPane.add(new JButton("MIDDLE"), BorderLayout.CENTER);
+
+    }
+
+    private void boxLayout()
+    {
+        this.contentPane.setLayout(new BoxLayout(contentPane, BoxLayout.Y_AXIS));
+
+        for (int i = 0; i < 23; i++)
+        {
+            this.add(new JButton("#" + i));
+        }
+    }
+
+    private void gridLayout()
+    {
+        this.contentPane.setLayout(new GridLayout(2, 5));
+
+        for (int i = 0; i < 10; i++)
+        {
+            this.contentPane.add(new JButton("#" + i));
+        }
+    }
+
+    private void customLayout()
+    {
+        this.contentPane.setLayout(new BorderLayout());
+
+        JPanel bottom = new JPanel();
+        bottom.setLayout(new BoxLayout(bottom, BoxLayout.X_AXIS));
+        bottom.add(one);
+        bottom.add(two);
+        bottom.add(three);
+        this.contentPane.add(bottom, BorderLayout.SOUTH);
+
+        JPanel twoButtons = new JPanel();
+        twoButtons.setLayout(new BoxLayout(twoButtons, BoxLayout.X_AXIS));
+        twoButtons.add(new JButton("LEFT"));
+        twoButtons.add(new JButton("RIGHT"));
+
+        JPanel middle = new JPanel();
+        middle.setLayout(new BoxLayout(middle, BoxLayout.Y_AXIS));
+        middle.add(new JButton("HI"));
+        middle.add(twoButtons);
+        middle.add(lab);
+        this.contentPane.add(middle, BorderLayout.CENTER);
+    }
+}
--- Week-4-GUI/Sec02/MainWindow.java
+++ Week-4-GUI/Sec02/MainWindow.java
@@ -1,4 +1,7 @@
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class MainWindow extends JFrame
 {
@@ -9,7 +12,32 @@ public class MainWindow extends JFrame
         super(windowName);
         clickCount = 0;
 
+        JPanel contentPane = new JPanel();
+        this.setContentPane(contentPane);
+
+        JLabel lab = new JLabel("Hi, I am a label!");
+        lab.setFont(new Font("Arial", 0, 40));
+        //lab.setPreferredSize(new Dimension(100, 100));
+
         JButton button = new JButton("I'm a button!");
-        this.add(button);
+        JButton buttonTwo = new JButton("I'm a better button!");
+        button.setPreferredSize(new Dimension(100, 100));
+
+        class InnerButtonListener implements ActionListener
+        {
+            @Override
+            public void actionPerformed(ActionEvent e)
+            {
+                System.out.println("You Pressed the button!");
+                lab.setFont(new Font("Arial", 1, 30));
+            }
+        }
+
+        buttonTwo.addActionListener(new InnerButtonListener());
+        //button.setSize(100,100);
+
+        contentPane.add(lab);
+        contentPane.add(button);
+        contentPane.add(buttonTwo);
     }
 }
